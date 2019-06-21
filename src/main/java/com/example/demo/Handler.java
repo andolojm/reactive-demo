97a9eb923f226a4dcc62eeac85167e140b82bffb
Jeff Andolora <jeeeeefff@gmail.com>
2019-06-21 12:20:59 -0500

    Cleanup

--- src/main/java/com/example/demo/Handler.java
+++ src/main/java/com/example/demo/Handler.java
@@ -1,17 +1,12 @@
 package com.example.demo;
 
 import static java.time.Duration.ofMillis;
-import static reactor.core.publisher.Flux.range;
-import static reactor.core.publisher.Mono.delay;
-
-import java.util.Random;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 
 // The handler
@@ -32,7 +27,6 @@ public class Handler {
   public Mono<ServerResponse> process(ServerRequest request) {
     return request
       .bodyToMono(String.class)
-      .doOnEach(requestBody -> System.out.println(requestBody))
       .flatMap(service::call)
       .flatMap(service::poll)
       .retryBackoff(retryCount, ofMillis(retryIntervalMs), ofMillis(retryIntervalMs))
--- src/main/java/com/example/demo/Service.java
+++ src/main/java/com/example/demo/Service.java
@@ -34,5 +34,4 @@ public class Service {
       .retrieve()
       .bodyToMono(String.class);
   }
-
 }
